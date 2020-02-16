d5ea1232a4e826072522a7aca81b67b58beb81d3
Michael.Owens <60488970+kanyenorthwest@users.noreply.github.com>
2020-02-17 10:27:03 +1300

    calculates movie prices

---
+++ movie_prices.py
@@ -0,0 +1,12 @@
+# this program calculates movie prices based off your age 
+EIGHTEEN_PRICE = 12
+THIRTEEN_PRICE = 9
+SIX_PRICE = 7
+FIVE_PRICE = 0
+
+# asks for the age of the customer
+age = int(input("What is your age sir? "))
+if age >= EIGHTEEN_PRICE:
+    print("Okey dokey your price do be {} dollars my good man.".format(EIGHTEEN_PRICE))
+
+
--- paracetomol.py
+++ paracetomol.py
@@ -1,2 +1,19 @@
+AGE_BOUNDARY = 12
+ADULT_DOSAGE = 500
+MILLIGRAMS_PER_KG = 10
+
+# asks for age and weight of patient
 age = int(input("What is the patient's age? "))
 weight = int(input("What is the patient's weight? "))
+
+# if the patient is 12 or over it is 500 mg paracetamol tablets
+if age >= AGE_BOUNDARY:
+    print("Take 2x doses of {}mg paracetamol tablets.".format(ADULT_DOSAGE))
+elif age < AGE_BOUNDARY:
+    dosage = MILLIGRAMS_PER_KG * weight
+    print("Recommended dosage is {}mg paracetamol tablets.".format(MILLIGRAMS_PER_KG * weight))
+    
+    
+    
+    
+    #ryan stewart has a pet mouse
